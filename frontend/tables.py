54f3083a8e5df382fb402448fc3e3644240fb3f0
João Pedro Pimentel <137961559+joaoppimentel@users.noreply.github.com>
2025-06-10 15:55:16 -0300

    Temporary Commit

--- frontend/tables.py
+++ frontend/tables.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import pandas as pd
-from utils.database import rename_columns, execute_query
+from utils.database import execute_query
+from utils.table_utils import prepare_dataframe
 
-st.title("Tabela ANAC ✈️")
+st.title("Tabelas ANAC ✈️")
+st.divider()
 
+st.header("Tabela Detalhada")
 df = execute_query("SELECT * FROM RelatorioVoosDetalhado", return_columns=True, fetch=True)
+df = prepare_dataframe(df)
+st.write(df)
+st.divider()
+
+st.header("Tabela Aeroportos")
+df = execute_query("SELECT * FROM aeroportos", return_columns=True, fetch=True)
 st.dataframe(df)
+st.divider()
---
+++ utils/table_utils.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def prepare_dataframe(dict):
+    df = pd.DataFrame(dict)
+    return df
+
+
