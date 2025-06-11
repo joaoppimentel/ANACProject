cd7531b78434adfcc620c92746f870e0a1dd0d4b
joaoppimentel <137961559+joaoppimentel@users.noreply.github.com>
2025-06-11 15:57:28 -0300

    Filters finalized

--- frontend/tables.py
+++ frontend/tables.py
@@ -1,18 +1,7 @@
 import streamlit as st
-import pandas as pd
-from utils.database import execute_query
-from utils.table_utils import prepare_dataframe
+from utils.table_utils import raw_tables
 
 st.title("Tabelas ANAC ✈️")
 st.divider()
 
-st.header("Tabela Detalhada")
-df = execute_query("SELECT * FROM RelatorioVoosDetalhado", return_columns=True, fetch=True)
-df = prepare_dataframe(df)
-st.write(df)
-st.divider()
-
-st.header("Tabela Aeroportos")
-df = execute_query("SELECT * FROM aeroportos", return_columns=True, fetch=True)
-st.dataframe(df)
-st.divider()
+raw_tables()
