9ce90465ea7f2fdac6253cbceafe6b48bd420213
João Pedro Pimentel <137961559+joaoppimentel@users.noreply.github.com>
2025-06-12 15:53:30 -0300

    start map def

--- frontend/graphs.py
+++ frontend/graphs.py
@@ -1,10 +1,22 @@
 import streamlit as st
+from utils.flies_map import render_map
 from utils.graph_utils import mostrar_big_numbers, mostrar_graficos, aplicar_filtro_mensal, mostrar_comparativo_mensal_percentual
 
-st.title("📈 Big Numbers e Gráficos ANAC 📊")
+big_numbers, graphs, flies_map = st.tabs(["Big Numbers", "Gráficos", "Mapa de Voos"])
 
 df = aplicar_filtro_mensal()
-st.dataframe(df)
-mostrar_big_numbers(df)
-mostrar_comparativo_mensal_percentual(df)
-mostrar_graficos(df)
+with big_numbers:
+    st.title("📈 Big Numbers📊")
+
+    mostrar_big_numbers(df)
+    st.dataframe(df)
+with graphs:
+    st.title("📈 Gráficos ANAC 📊")
+
+    mostrar_comparativo_mensal_percentual(df)
+    mostrar_graficos(df)
+
+with flies_map:
+    st.title("🗺️Mapa Voos🛫")
+
+    render_map()
---
+++ utils/flies_map.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def render_map():
+    st.write("Teste")
